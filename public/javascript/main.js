d559dd78c70c01879a62f79368ae5910801e9d27
kushkamisha <kushka.misha@gmail.com>
2019-03-23 22:54:28 +0200

    All works with events

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -49,7 +49,7 @@ $('#votes').click(function () {
     const address = $('#votesAddress').val()
     contractInstance.votes.call(address, (err, votes) => {
         if (err) console.error({ err })
-        console.log({ votes })
+        // console.log({ votes })
         $('#wantToBeOracle').text(votes[0])
         $('#votesForAddress').text(votes[1].toNumber())
     })
@@ -61,48 +61,65 @@ $('#becomeCandidate').click(function () {
             if (err) console.error({ err })
             contractInstance.becameCandidateToOracles({ from: address }, function(err, receipt) {
                 if (err) console.error({ err })
-                console.log({ receipt })
-                handleEvent()
+                // console.log({ receipt })
+                handleEvent('BecameCandidateToOracles')
+                    .then((event, err) => {
+                        if (err) console.error({ err })
+                        console.log({ event })
+                        $('#becomeCandidateStatus').text('true')
+                    })
             })
         })
 })
 
-// getAccount()
-//     .then((account, err) => {
-//         if (err) console.error({ err })
-//         contractInstance.setWelcomeString(newString, { from: account }, function(err, receipt) {
-//             if (err) console.error({ err })
-//             console.log({ receipt })
-//         })
-//     })
-
-function handleEvent() {
-    if (web3 && contractInstance) {
-        const BecameCandidateToOracles = contractInstance.BecameCandidateToOracles()
-        BecameCandidateToOracles.watch(function (err, res) {
-
+$('#vote').click(function () {
+    const forAddress = $('#voteAddress').val()
+    getAccount()
+        .then((address, err) => {
             if (err) console.error({ err })
-
-            //         Callback return
-            //         Object - An event object as follows:
-
-            //         address: String, 32 Bytes - address from which this log originated.
-            //         args: Object - The arguments coming from the event.
-            //         blockHash: String, 32 Bytes - hash of the block where this log was in. null when its pending.
-            //         blockNumber: Number - the block number where this log was in. null when its pending.
-            //         logIndex: Number - integer of the log index position in the block.
-            //         event: String - The event name.
-            //         removed: bool - indicate if the transaction this event was created from was removed from the blockchain (due to orphaned block) or never get to it (due to rejected transaction).
-            //         transactionIndex: Number - integer of the transactions index position log was created from.
-            //         transactionHash: String, 32 Bytes - hash of the transactions this log was created from.
-
-            // event arguments cointained in result.args object
-            const { candidateAddress } = res.args
-            console.log({ res })
-            // new data have arrived. it is good idea to udpate data & UI
-            // addNewDataToStateAndUpdateUI()
+            contractInstance.vote(forAddress, { from: address }, function (err, receipt) {
+                if (err) console.error({ err })
+                // console.log({ receipt })
+                handleEvent('Vote')
+                    .then((event, err) => {
+                        if (err) console.error({ err })
+                        console.log({ event })
+                        $('#voteForCandidate').text('true')
+                    })
+            })
         })
-    }
+})
+
+function handleEvent(eventName) {
+    return new Promise((resolve, reject) => {
+        if (web3 && contractInstance) {
+            const event = contractInstance[eventName]()
+            event.watch(function (err, res) {
+
+                if (err) reject(err)
+
+                //         Callback return
+                //         Object - An event object as follows:
+
+                //         address: String, 32 Bytes - address from which this log originated.
+                //         args: Object - The arguments coming from the event.
+                //         blockHash: String, 32 Bytes - hash of the block where this log was in. null when its pending.
+                //         blockNumber: Number - the block number where this log was in. null when its pending.
+                //         logIndex: Number - integer of the log index position in the block.
+                //         event: String - The event name.
+                //         removed: bool - indicate if the transaction this event was created from was removed from the blockchain (due to orphaned block) or never get to it (due to rejected transaction).
+                //         transactionIndex: Number - integer of the transactions index position log was created from.
+                //         transactionHash: String, 32 Bytes - hash of the transactions this log was created from.
+
+                // event arguments cointained in result.args object
+                // const { candidateAddress } = res.args
+                // console.log({ res })
+                resolve(res)
+                // new data have arrived. it is good idea to udpate data & UI
+                // addNewDataToStateAndUpdateUI()
+            })
+        }
+    })
 }
 
 function oraclesCount() {
