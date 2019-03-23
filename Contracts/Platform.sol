b3a8947b54436acf140dcf3e56f0c29a80350d83
DenyStark <denyskrolevetskyi@gmail.com>
2019-03-23 22:44:52 +0200

    Ref: Platrorm > remove getOraclesReward function

--- Contracts/Platform.sol
+++ Contracts/Platform.sol
@@ -10,19 +10,8 @@ contract Platform is PlatformInterface, DPOS {
 
     uint16 mRevardMultiplier = 100; // revardMultiplier in milli (10e3)
     mapping (address => uint) public oraclesBalance;
-    
-    event GetOraclesReward(address oracle, uint amount);
+
     event WithdrawOraclesReward(address oracle, uint amount);
-
-    function getOraclesReward(uint totalReward) private returns(bool) {
-        uint oracleRevard = (totalReward / oraclesCount) * mRevardMultiplier / 1000;
-        for (uint8 i = 0; i < oraclesCount; i++) {
-            oraclesBalance[oracles[i]] += oracleRevard;
-            emit GetOraclesReward(oracles[i], oracleRevard);
-        }
-
-        return true;
-    }
 
     function withdrawOraclesReward(uint amount) external payable returns(bool) {
         if (oraclesBalance[msg.sender] > amount) {
