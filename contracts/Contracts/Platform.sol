f3fce1fb3081b4917fdb5792bb3ff525f34034ff
DenyStark <denyskrolevetskyi@gmail.com>
2019-03-24 12:37:20 +0200

    Style: Platform > naming

--- contracts/Contracts/Platform.sol
+++ contracts/Contracts/Platform.sol
@@ -3,7 +3,7 @@ pragma solidity 0.5.6;
 import "browser/DPOS.sol";
 
 interface PlatformInterface {
-    function withdrawOraclesReward(uint amount) external payable returns(bool);
+    function withdrawOracleReward(uint amount) external payable returns(bool);
 }
 
 contract Platform is PlatformInterface, DPOS {
@@ -11,13 +11,13 @@ contract Platform is PlatformInterface, DPOS {
     uint16 mRevardMultiplier = 100; // revardMultiplier in milli (10e3)
     mapping (address => uint) public oraclesBalance;
 
-    event WithdrawOraclesReward(address oracle, uint amount);
+    event WithdrawOracleReward(address oracle, uint amount);
 
-    function withdrawOraclesReward(uint amount) external payable returns(bool) {
+    function withdrawOracleReward(uint amount) external payable returns(bool) {
         if (oraclesBalance[msg.sender] > amount) {
             oraclesBalance[msg.sender] = oraclesBalance[msg.sender].sub(amount);
 
-            emit WithdrawOraclesReward(msg.sender, amount);
+            emit WithdrawOracleReward(msg.sender, amount);
             msg.sender.transfer(amount);
         }
 
